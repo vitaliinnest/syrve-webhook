22cfcff4e5c2e4e89ab089b2905a9508f4b16825
Yevhen <lebedenko077@gmail.com>
2022-10-10 20:30:29 +0200

    Routes

--- routes.ts
+++ routes.ts
@@ -3,5 +3,6 @@ import { Express } from "express";
 import controllers from "./controllers";
 
 export default (app: Express) => {
+    app.get('/syrve/webhook', controllers.syrve.webhook)
     app.post('/syrve/webhook', controllers.syrve.webhook)
 }
