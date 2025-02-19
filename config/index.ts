3ca365625e89d64e42c0cddbfc77d83fb0b01e58
vitaliinnest <vitaliinnest@gmail.com>
2025-02-19 09:27:16 +0200

    refactor: remove unused menu language configuration from config

--- config/index.ts
+++ config/index.ts
@@ -4,10 +4,6 @@ interface IConfig {
         apiLogin: string;
         organizationId: string;
         terminalGroupId: string;
-        menu_lang: {
-            UKR: string;
-            RUS: string;
-        };
         streets: {
             undefined: string;
         };
@@ -38,10 +34,6 @@ const config: IConfig = {
         apiLogin: "f305a494-3cd",
         organizationId: "ecaebd61-e4f7-46b7-ab02-8367249446e0",
         terminalGroupId: "a556d82b-38a9-d371-016f-3c7cfc8500cc",
-        menu_lang: {
-            UKR: "fa826cdd-9b44-4cd7-b1c7-f2ed736b956e",
-            RUS: "66b85226-49f8-43fd-9764-a4da6a3cc178",
-        },
         streets: {
             undefined: "78d6bf50-164e-408e-9638-c0133ea3c320",
         },
