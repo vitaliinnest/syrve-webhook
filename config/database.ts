8aaf48aaa432d8acc2a22e67887ed4c60fa8994b
vitaliinnest <vitaliinnest@gmail.com>
2025-02-19 09:26:29 +0200

    feat: enhance database configuration to include nomenclature retrieval and mapping

--- config/database.ts
+++ config/database.ts
@@ -1,8 +1,9 @@
 import SyrveApi from "../modules/SyrveApi";
+import { ISyrveNomenclatureSpace } from "../types";
 
 const config: any = {
     streets: { load: () => loadStreets(), data: null },
-    nomenclature: { load: () => SyrveApi.nomenclature(), data: null }
+    nomenclature: { load: () => nomenclatureDb.get(), data: null }
 }
 
 export const database = {
@@ -13,7 +14,18 @@ export const database = {
             config[key].data = await load();
         }
     },
-    get: (param: string) => config[param].data
+    get: (param: string) => config[param].data,
+}
+
+export const nomenclatureDb = {
+    get: (): ISyrveNomenclatureSpace.RootObject  => {
+        const nomenclature =  database.get('nomenclature') as ISyrveNomenclatureSpace.RootObject;
+        nomenclature.productByIdMap = nomenclature.products.reduce((map, product) => {
+            map[product.id] = product;
+            return map;
+        }, {} as { [key: string]: ISyrveNomenclatureSpace.Product });
+        return nomenclature;
+    }
 }
 
 async function loadStreets() {
